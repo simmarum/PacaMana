0760e315c251bc055bfb130c2faf4d44e9897647
simmarum <mateusz0831@gmail.com>
2017-06-10 14:11:42 +0200

    Nowy model PAC-MANA -> Animowany ;d

--- player.cpp
+++ player.cpp
@@ -5,20 +5,33 @@
 #include "loaderOBJ.h"
 
 
-#define mPlayer "modeleBlend/pacman.obj"
+#define mPlayer1 "modeleBlend/pacman1.obj"
+#define mPlayer2 "modeleBlend/pacman2.obj"
 
 using namespace glm;
 
 Player::Player(Map* &mapa,colision_length &colision_length) {
-    bool res = loadOBJ(mPlayer, this->TEMPvertices, this->TEMPuvs, this->TEMPnormals,this->TEMPvCount,colision_length);
+    bool res = loadOBJ(mPlayer1, this->TEMPvertices, this->TEMPuvs, this->TEMPnormals,this->TEMPvCount,colision_length);
     if(!res) {
         printf("Nie uda³o siê wczytaæ!");
         exit(1);
     }
     position = vec3(1.0,1.0,1.0);
-    rotation = vec3(0.0,0.0,0.0);
+    rotation = vec3(0.0,0.0,-10.0f*PI/180.0f);
     scale = vec3(1.0,1.0,1.0);
     speed = 2;
+
+    struct colision_length unusable;
+    res = loadOBJ(mPlayer2, this->TEMPvertices2, this->TEMPuvs2, this->TEMPnormals2,this->TEMPvCount2,unusable);
+    if(!res) {
+        printf("Nie uda³o siê wczytaæ!");
+        exit(1);
+    }
+    position2 = position;
+    rotation2 = rotation;
+    scale2 = vec3(0.99f,0.99f,0.99f);
+    speed2 = 4;
+    rotation_temp = rotation2.z;
     findPosition(mapa);
 }
 
@@ -41,6 +54,11 @@ void Player::findPosition(Map* &mapa) {
 
 
 void Player::drawSolid(GLuint &tex,mat4 &V) {
+    this->drawSolid_1(tex,V);
+    this->drawSolid_2(tex,V);
+}
+
+void Player::drawSolid_1(GLuint &tex,mat4 &V) {
     glEnable(GL_NORMALIZE);
 
     glEnableClientState(GL_VERTEX_ARRAY);
@@ -51,7 +69,8 @@ void Player::drawSolid(GLuint &tex,mat4 &V) {
 
     mat4 M=mat4(1.0f);
     M=translate(M,this->position);
-    M=rotate(M,this->rotation.x,vec3(1.0,0.0,0.0));
+    M=rotate(M,this->rotation.y,vec3(0.0,1.0,0.0));
+    M=rotate(M,this->rotation.z,vec3(0.0,0.0,1.0));
     glLoadMatrixf(value_ptr(V*M));
 
     glVertexPointer(3,GL_FLOAT,0,&(this->TEMPvertices[0]));
@@ -70,6 +89,46 @@ void Player::drawSolid(GLuint &tex,mat4 &V) {
     glMaterialf(GL_FRONT_AND_BACK, GL_SHININESS, shininess);
 
     glDrawArrays(GL_TRIANGLES,0,this->TEMPvCount);
+
+    glDisableClientState(GL_VERTEX_ARRAY);
+    glDisableClientState(GL_NORMAL_ARRAY);
+    glDisableClientState(GL_TEXTURE_COORD_ARRAY);
+}
+void Player::drawSolid_2(GLuint &tex,mat4 &V) {
+    glEnable(GL_NORMALIZE);
+
+    glEnableClientState(GL_VERTEX_ARRAY);
+    glEnableClientState(GL_NORMAL_ARRAY);
+    glEnableClientState(GL_TEXTURE_COORD_ARRAY);
+
+    glBindTexture(GL_TEXTURE_2D,tex);
+
+    mat4 M=mat4(1.0f);
+    M=translate(M,this->position);
+    M=rotate(M,this->rotation.y,vec3(0.0,1.0,0.0));
+    this->rotation_temp -= (float) (glfwGetTime()*speed2);
+    this->rotation_temp = (float) (this->rotation_temp - 2*PI*(ceil(this->rotation_temp/(2*PI))));
+    this->rotation2.z = (float)((sin(this->rotation_temp)-1)*0.5);
+    this->rotation2.z += this->rotation.z;
+    M=rotate(M,this->rotation2.z,vec3(0.0,0.0,1.0));
+    glLoadMatrixf(value_ptr(V*M));
+
+    glVertexPointer(3,GL_FLOAT,0,&(this->TEMPvertices2[0]));
+    glNormalPointer(GL_FLOAT,sizeof(float)*3,&(this->TEMPnormals2[0]));
+    glTexCoordPointer(2,GL_FLOAT,0,&(this->TEMPuvs2[0]));
+
+    float ambient[] = {0,0,0,1};
+    float emision[] = {0,0,0,1};
+    float diffuse[] = {0.7,0.5,0.5,1};
+    float specular[] = {0.5,0.5,0.5,1};
+    float shininess = 50;
+    glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, ambient);
+    glMaterialfv(GL_FRONT_AND_BACK, GL_EMISSION, emision);
+    glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, diffuse);
+    glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, specular);
+    glMaterialf(GL_FRONT_AND_BACK, GL_SHININESS, shininess);
+
+    glDrawArrays(GL_TRIANGLES,0,this->TEMPvCount2);
 
     glDisableClientState(GL_VERTEX_ARRAY);
     glDisableClientState(GL_NORMAL_ARRAY);
